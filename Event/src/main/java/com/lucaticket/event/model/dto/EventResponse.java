7247bac651d20b67a86bdd5fe0a98df519a70bf0
AngelZhang159 <angelzhang159@gmail.com>
2024-12-10 15:42:20 +0100

    Creado DTO event-response

--- Event/src/main/java/com/lucaticket/event/model/dto/EventResponse.java
+++ Event/src/main/java/com/lucaticket/event/model/dto/EventResponse.java
@@ -1,5 +1,25 @@
 package com.lucaticket.event.model.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class EventResponse {
 
+    private String name;
+
+    private String description;
+
+    private LocalDateTime eventDate;
+    
+    private double minPrice;
+
+    private double maxPrice;
+
+    private String location;
+
+    private String venueName;
+
+    private Genre genre;
 }
