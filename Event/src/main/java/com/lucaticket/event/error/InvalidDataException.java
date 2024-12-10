a7a8cc060b52be4c9897468df39ab8cce6c76e5a
Alberto de la Blanca <alberto.delablanca@gmail.com>
2024-12-10 16:34:03 +0100

    lt 27 invalid data exception

--- Event/src/main/java/com/lucaticket/event/error/InvalidDataException.java
+++ Event/src/main/java/com/lucaticket/event/error/InvalidDataException.java
@@ -1,5 +1,25 @@
 package com.lucaticket.event.error;
 
-public class InvalidDataException {
+/**
+ * Excepción personalizada para indicar que los datos proporcionados son inválidos.
+ */
+public class InvalidDataException extends RuntimeException {
 
-}
+    /**
+     * Constructor que acepta un mensaje de error.
+     *
+     * @param message El mensaje detallado de la excepción.
+     */
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor que acepta un mensaje de error y una causa.
+     *
+     * @param message El mensaje detallado de la excepción.
+     * @param cause   La causa original de la excepción.
+     */
+    public InvalidDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
