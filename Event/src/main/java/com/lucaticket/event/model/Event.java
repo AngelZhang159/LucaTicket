67f8ba68d18cd850df2b9e9adc8b7df5c6d2a45a
Alberto de la Blanca <alberto.delablanca@gmail.com>
2024-12-10 15:55:56 +0100

    .

--- Event/src/main/java/com/lucaticket/event/model/Event.java
+++ Event/src/main/java/com/lucaticket/event/model/Event.java
@@ -1,5 +1,41 @@
 package com.lucaticket.event.model;
 
+
+//@Alberto
+
 public class Event {
+	
+	private long id;
+	private String name;
+	private String description;
+	private Date evantDate;
+	private LocalTime eventTime;
+	private double minPrice;
+	private double maxPrice;
+	private String location;
+	private String venueName
+	private String genere;
+	
+	//Empty constructor
+	public Event() {
+		
+	}
+	
+	//Constructor
+    public Event(long id, String name, String description, Date eventDate, LocalTime eventTime, 
+            double minPrice, double maxPrice, String location, String venueName, String genre) {
+	   this.id = id;
+	   this.name = name;
+	   this.description = description;
+	   this.eventDate = eventDate;
+	   this.eventTime = eventTime;
+	   this.minPrice = minPrice;
+	   this.maxPrice = maxPrice;
+	   this.location = location;
+	   this.venueName = venueName;
+	   this.genre = genre;
+    }
+    
+    
 
 }
