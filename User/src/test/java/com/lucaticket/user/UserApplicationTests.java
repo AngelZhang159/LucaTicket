d033330b13cf15b3dacffb25373607451511b673
Raul <raulvalentinradu@gmail.com>
2024-12-11 10:28:24 +0100

    adding test for invalid data exception throwing

--- User/src/test/java/com/lucaticket/user/UserApplicationTests.java
+++ User/src/test/java/com/lucaticket/user/UserApplicationTests.java
@@ -1,13 +1,60 @@
 package com.lucaticket.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.lucaticket.user.error.InvalidDataException;
+import com.lucaticket.user.model.User;
+import com.lucaticket.user.model.dto.UserRequest;
+import com.lucaticket.user.model.dto.UserResponse;
+import com.lucaticket.user.repository.UserRepository;
+import com.lucaticket.user.service.impl.UserServiceImpl;
+import com.sun.jdi.request.EventRequest;
 
 @SpringBootTest
-class UserApplicationTests {
+class EventApplicationTests {
+	// <-- Atributos -->
 
-	@Test
-	void contextLoads() {
+	@InjectMocks
+	private UserServiceImpl userService;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
+	@Test
+	void should_throw_invalid_data_exception_when_wrong_request() {
+		
+		when(userService.saveUser(any(UserRequest.class))).thenThrow(InvalidDataException.class);
+		
+		assertThrows(InvalidDataException.class, () -> userService.saveUser(new UserRequest()),
+				"Debería lanzarse InvalidDataException cuando el DTO tiene datos inválidos.");
+	}
 }
