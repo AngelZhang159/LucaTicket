95073814485b50d142f637451b1673798a6eeaa7
AngelZhang159 <angelzhang159@gmail.com>
2024-12-11 16:22:37 +0100

    Detail de un evento en controller

--- Event/src/main/java/com/lucaticket/event/controller/EventController.java
+++ Event/src/main/java/com/lucaticket/event/controller/EventController.java
@@ -8,6 +8,7 @@ import com.lucaticket.event.model.dto.EventResponse;
 import com.lucaticket.event.service.EventService;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -15,38 +16,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @Slf4j
 @RequestMapping("/event")
+@RequiredArgsConstructor
 public class EventController {
 
-    @Autowired
-    private EventService eventService;
+	private final EventService eventService;
 
-    /*
-     * @Olivord
-     * Guarda el evento en la base de datos
-     */
-    @PostMapping
-    public ResponseEntity<EventResponse> saveEvent(@RequestBody @Valid EventRequest eventoRequest) {
-        log.info("EventController.saveEvent");
-        return eventService.saveEvent(eventoRequest);
-    }
-    
-    /**
-     * @author Raul
-     * @return una lista con todos los eventos
-     */
-    @GetMapping("/listAll")
-    public ResponseEntity<List<EventResponse>> getEvents() {
-    	log.info("EventController.getEvents");
-    	return eventService.getEvents();
-    }
-    
+	/*
+	 * @Olivord Guarda el evento en la base de datos
+	 */
+	@PostMapping
+	public ResponseEntity<EventResponse> saveEvent(@RequestBody @Valid EventRequest eventoRequest) {
+		log.info("EventController.saveEvent");
+		return eventService.saveEvent(eventoRequest);
+	}
+
+	/**
+	 * @author Raul
+	 * @return una lista con todos los eventos
+	 */
+	@GetMapping("/listAll")
+	public ResponseEntity<List<EventResponse>> getEvents() {
+		log.info("EventController.getEvents");
+		return eventService.getEvents();
+	}
+	
+	/**
+	 * @author Angel
+	 * @param id
+	 * @return el evento con todos los detalles
+	 */
+
+	@GetMapping("/detail/{id}")
+	public ResponseEntity<DetailEventResponse> getDetail(@PathVariable long id) {
+		return eventService.getDetailedInfoEvent(id);
+	}
+
 }
