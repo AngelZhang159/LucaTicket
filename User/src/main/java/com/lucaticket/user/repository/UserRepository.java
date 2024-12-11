8936339e8ea8b153efa1db3074073e95dd112343
AngelZhang159 <angelzhang159@gmail.com>
2024-12-11 09:39:47 +0100

    user repo jpa

--- User/src/main/java/com/lucaticket/user/repository/UserRepository.java
+++ User/src/main/java/com/lucaticket/user/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.lucaticket.user.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucaticket.user.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
