9ae5988b041fc95a52565310872d4e955e681ae4
Alberto de la Blanca <alberto.delablanca@gmail.com>
2024-12-13 13:00:54 +0100

    lt 119 listTickets()

--- TicketService/src/main/java/com/lucaticket/ticketservice/service/TicketService.java
+++ TicketService/src/main/java/com/lucaticket/ticketservice/service/TicketService.java
@@ -1,8 +1,11 @@
 package com.lucaticket.ticketservice.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.lucaticket.ticketservice.model.Ticket;
 import com.lucaticket.ticketservice.model.dto.TicketRequest;
 import com.lucaticket.ticketservice.model.dto.TicketResponse;
 import com.lucaticket.ticketservice.repository.TicketRepository;
@@ -12,4 +15,13 @@ import lombok.RequiredArgsConstructor;
 public interface TicketService {
 
 	public ResponseEntity<TicketResponse> save(TicketRequest ticketRequest);
+	
+    /**
+     * @author Alberto de la Blanca
+     * Recupera una lista de todos los tickets disponibles en el sistema.
+     * 
+     * @return ResponseEntity que contiene una lista de tickets.
+     */
+	
+	ResponseEntity<List<Ticket>> listTickets();
 }
