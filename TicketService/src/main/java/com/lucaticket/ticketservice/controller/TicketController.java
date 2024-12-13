da2413f9e26f2b676337ae9c327ccee7e0adcbe6
AngelZhang159 <angelzhang159@gmail.com>
2024-12-13 12:58:20 +0100

    hotfix controller

--- TicketService/src/main/java/com/lucaticket/ticketservice/controller/TicketController.java
+++ TicketService/src/main/java/com/lucaticket/ticketservice/controller/TicketController.java
@@ -1,23 +1,35 @@
 package com.lucaticket.ticketservice.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucaticket.ticketservice.model.dto.TicketRequest;
+import com.lucaticket.ticketservice.model.dto.TicketResponse;
+import com.lucaticket.ticketservice.service.TicketService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TicketController {
-	
+
+	private final TicketService ticketService;
+
 	/**
 	 * @author Raul
 	 * @param un objeto con los parametros necesarios para guardar un ticket
-	 * @return una entidad de respuesta con un codigo http y el objeto ticket que se ha guardado
+	 * @return una entidad de respuesta con un codigo http y el objeto ticket que se
+	 *         ha guardado
 	 */
-	@PostMapping("/ticket") 
+	@PostMapping("/ticket")
 	ResponseEntity<TicketResponse> save(TicketRequest ticket) {
 		return ticketService.save(ticket);
 	}
-	
+
 	/**
 	 * @author Raul
 	 * @return una lista con todos los tickets registrados
@@ -26,5 +38,5 @@ public class TicketController {
 	ResponseEntity<List<TicketResponse>> listTickets() {
 		return ticketService.listTickets();
 	}
-	
+
 }
