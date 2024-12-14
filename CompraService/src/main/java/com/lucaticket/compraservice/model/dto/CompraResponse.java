1c12ba4bb2df746bc4ed16b6b8cf394d405eab54
Raul <raulvalentinradu@gmail.com>
2024-12-14 13:10:02 +0100

    creating response DTO for compra

--- CompraService/src/main/java/com/lucaticket/compraservice/model/dto/CompraResponse.java
+++ CompraService/src/main/java/com/lucaticket/compraservice/model/dto/CompraResponse.java
@@ -1,5 +1,15 @@
 package com.lucaticket.compraservice.model.dto;
 
-public class CompraResponse {
+import java.time.LocalDateTime;
 
+public class CompraResponse {
+	
+	private final String mensaje = "Compra realizada !";
+	private String ownerName;
+	private String email;
+	private Long idEvento;
+	private String nombreEvento;
+	private Double price;
+	private LocalDateTime fechaEvento;
+	
 }
