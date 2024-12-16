cfca4a667168b74e003e8f58841445c0b64eab33
Raul <raulvalentinradu@gmail.com>
2024-12-16 16:42:41 +0100

    creating ticket response

---
+++ CompraService/src/main/java/com/lucaticket/compraservice/model/dto/TicketResponse.java
@@ -0,0 +1,22 @@
+package com.lucaticket.compraservice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketResponse {
+
+	/**
+	 * Correo electrónico del usuario asociado al ticket.
+	 */
+	private String email;
+
+	/**
+	 * Identificador del evento al que pertenece este ticket.
+	 */
+	private Long idEvent;
+
+}
