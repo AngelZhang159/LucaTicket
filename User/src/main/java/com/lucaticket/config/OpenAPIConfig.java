a38df4b963cabd2813dc89ad5d48eadc7cafc2c1
Angel Zhang <angelzhang159@gmail.com>
2024-12-17 18:50:00 +0100

    docs

---
+++ User/src/main/java/com/lucaticket/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.lucaticket.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Grupo 2" 
+						),
+				title = "LucaTicket Spring",
+				description = "Documentación del projecto 3 [LucaTicket], microservicio de USUARIO",
+				version = "1.0"
+				),
+		servers = {
+				@Server(
+						description = "Local",
+						url = "http://localhost:8082"
+				),
+				
+		}
+)
+public class OpenAPIConfig {
+
+}
